070f48ee1f9a09dcde18190fb2ab652db98109d6
zhaoyang <18355403288@163.com>
2018-12-09 19:02:00 +0800

    my community

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/home/index.vue';
+import Community from './views/community/index.vue';
+import My from './views/my/index.vue';
 
 Vue.use(Router);
 
@@ -12,6 +14,16 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
+    },
+    {
+      path: '/community',
+      name: 'community',
+      component: Community,
+    },
+    {
+      path: '/my',
+      name: 'my',
+      component: My,
     },
     {
       path: '/about',
