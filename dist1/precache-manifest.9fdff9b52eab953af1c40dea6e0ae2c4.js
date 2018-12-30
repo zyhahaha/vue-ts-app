3d93b5c324bd77de614228a44661fb4e13e45d01
zhaoyang <18355403288@163.com>
2018-12-30 21:48:49 +0800

    dist

---
+++ dist1/precache-manifest.9fdff9b52eab953af1c40dea6e0ae2c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "43dbb0297eda5596a9ec",
+    "url": "/js/chunk-vendors.83797f13.js"
+  },
+  {
+    "revision": "e1e033d29dd8adbb914d",
+    "url": "/js/app.a4da7485.js"
+  },
+  {
+    "revision": "fd7614bcfe99bdcc11822c2302f326b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1e033d29dd8adbb914d",
+    "url": "/css/app.bd19708d.css"
+  }
+];
